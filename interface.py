11ba7d3b34dbdb025cfb94b0a8146fee2be9c1b9
ph_santosjr <paulo.henrique@databrain.com.br>
2022-11-22 10:04:42 -0300

    Update interface

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
-from abc import ABC
+from abc import ABC, abstractmethod
 
 @dataclass
 class CoffeeMaker(ABC):
+    @abstractmethod
     def brew(self, make: str):
         print(f'Making your coffee in {make}')
 
